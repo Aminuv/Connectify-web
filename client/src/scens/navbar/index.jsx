44e0a072d22f5db9d0e8128a7753df8ddd86556e
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-03-26 20:47:39 +0000

    Create and consolidate interface files in src folder

---
+++ client/src/scens/navbar/index.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
