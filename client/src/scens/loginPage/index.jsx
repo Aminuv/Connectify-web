3205d0c002e1482951c01ec360a6db40bb948554
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-03-28 18:40:03 +0000

    runing the server and fix the login & sregister page

--- client/src/scens/loginPage/index.jsx
+++ client/src/scens/loginPage/index.jsx
@@ -14,7 +14,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-        Connectify
+         Connectify
         </Typography>
       </Box>
 
@@ -26,9 +26,9 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Connectify! 
+          Welcome to Socipedia, the Social Media for Connectify!
         </Typography>
-        <Form />      
+        <Form />
       </Box>
     </Box>
   );
