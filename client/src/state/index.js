c0ff350d9a8374c6cfba16749d7a5a7ce7566467
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-03-29 17:17:57 +0000

    complite the wedjet page and fix some errors

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -12,36 +12,36 @@ export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        setMode: (state) => {
-            state.mode = state.mode === "light" ? "dark" : "light";
-        },
-        sztLogin: (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-        },
-        setLogin: (state) => {
-            state.user = null;
-            state.token = null;
-        },
-        setFriends:(state, action) => {
-            if (state.user) {
-                state.user.friends = action.payload.friends;
-            } else {
-                console.error("User is not logged in!");
-            }
-        },
-        setPosts: (state, action) => {
-            state.posts = action.payload.posts;
-        },
-        setPosts: (state, action) => {
-            const updatePost = action.payload.map((post) => {
-                if (post._id === action.payload.post_id) return action.payload.post;
-                return post;
-            });
-        state.posts = updatePost;
-        },
-    }, 
-},);
+      setMode: (state) => {
+        state.mode = state.mode === "light" ? "dark" : "light";
+      },
+      setLogin: (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      },
+      setLogout: (state) => {
+        state.user = null;
+        state.token = null;
+      },
+      setFriends: (state, action) => {
+        if (state.user) {
+          state.user.friends = action.payload.friends;
+        } else {
+          console.error("user friends non-existent :(");
+        }
+      },
+      setPosts: (state, action) => {
+        state.posts = action.payload.posts;
+      },
+      setPost: (state, action) => {
+        const updatedPosts = state.posts.map((post) => {
+          if (post._id === action.payload.post._id) return action.payload.post;
+          return post;
+        });
+        state.posts = updatedPosts;
+      },
+    },
+});
 // Export the action creators
 export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
 
