e763b9dcef9727bc3e3c80d403a87948881ca77c
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-03-29 16:26:40 +0000

    starting by th component that displays posts

--- client/src/scens/widgets/PostsWidget.jsx
+++ client/src/scens/widgets/PostsWidget.jsx
@@ -5,7 +5,74 @@ import PostWidget from "./PostWidget";
 
 
 // A component that displays posts.
-const PostsWidget = ({ userId, isProfile = false }) => {};
-
-
-export default PostsWidget;
+const PostsWidget = ({ userId, isProfile = false }) => {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts);
+    const token = useSelector((state) => state.token);
+  
+    // Fetch posts from the server
+    const getPosts = async () => {
+      const response = await fetch("http://localhost:3001/posts", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
+  
+    // Fetch user posts from the server
+    const getUserPosts = async () => {
+      const response = await fetch(
+        `http://localhost:3001/posts/${userId}/posts`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
+  
+    // Fetch posts when the component is mounted
+    useEffect(() => {
+      if (isProfile) {
+        getUserPosts();
+      } else {
+        getPosts();
+      }
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  
+    return (
+      <>
+        {posts.map(
+          ({
+            _id,
+            userId,
+            firstName,
+            lastName,
+            description,
+            location,
+            picturePath,
+            userPicturePath,
+            likes,
+            comments,
+          }) => (
+            <PostWidget
+              key={_id}
+              postId={_id}
+              postUserId={userId}
+              name={`${firstName} ${lastName}`}
+              description={description}
+              location={location}
+              picturePath={picturePath}
+              userPicturePath={userPicturePath}
+              likes={likes}
+              comments={comments}
+            />
+          )
+        )}
+      </>
+    );
+  };
+  
+  export default PostsWidget;
