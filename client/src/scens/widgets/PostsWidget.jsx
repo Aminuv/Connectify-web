260174bc4252dd887f3e8dc202179f97e7e3f6e4
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-03-29 00:23:13 +0000

    statrt working on the component that displays posts

---
+++ client/src/scens/widgets/PostsWidget.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+
+// A component that displays posts.
+const PostsWidget = ({ userId, isProfile = false }) => {};
+
+
+export default PostsWidget;
